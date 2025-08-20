8d5566bd3df70c61f6c0a57d8f1908f0b46893ab
GeraltJM <jesus.voluntadheredada.human@gmail.com>
2025-08-20 15:20:27 -0600

    regresion

--- curso1/regresion.py
+++ curso1/regresion.py
@@ -10,9 +10,37 @@ Created on Tue Aug 19 07:08:13 2025
 import numpy as np # aplicacion de matematicas
 import matplotlib as plt # mostrar datos
 import pandas as pd # manipular datos
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
 
 
 dataset = pd.read_csv('C:/Users/jesus/IA/machinelearning/machinelearningbook/archivos/Data.csv')
 
 X = dataset.iloc[ :,:-1 ].values
 y = dataset.iloc[ :,3 ].values
+# axis = 0 columna, axis = 1 fila
+imputer = SimpleImputer(missing_values = np.nan, strategy="mean")
+
+imputer.fit(X[:,1:3])
+
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+labelEncoder_X = LabelEncoder()
+
+
+X[ :,0 ] = labelEncoder_X.fit_transform(X[ :,0 ])
+
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],    
+    remainder='passthrough'                         
+)
+ 
+
+X = np.array(ct.fit_transform(X),  dtype=float)
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split( X, y,  test_size=0.2, random_state=42)
--- regresionlineal.R
+++ regresionlineal.R
@@ -1,3 +1,34 @@
 
 
 dataset = read.csv('C:/Users/jesus/IA/machinelearning/machinelearningbook/archivos/Data.csv')
+
+
+# Tratamientos valores NAN
+dataset$Age = ifelse( is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Age  )
+
+
+dataset$Salary = ifelse( is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Salary  )
+
+
+dataset$Country = factor( dataset$Country, 
+                          levels = c("France", "Spain", "Germany"), 
+                          labels = c(1,2,3) )
+
+
+dataset$Purchased = factor( dataset$Purchased, 
+                          levels = c("No", "Yes"), 
+                          labels = c(0,1) )
+
+#install.packages("caTools")
+
+set.seed(123)
+split = sample.split( dataset$Purchased, SplitRatio = 0.8 )
+training_set = subset( dataset, split == TRUE  )
+testing_set = subset( dataset, split == FALSE  )
+
+
+
+
+
+
+
