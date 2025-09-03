ce2c5a0f615b215adb7d975d9a6822a8951e7d78
GeraltJM <jesus.voluntadheredada.human@gmail.com>
2025-09-03 08:31:16 -0600

    kernel svm

---
+++ kernelsvm.R
@@ -0,0 +1,57 @@
+dataset = read.csv('C:/Users/jesus/IA/machinelearning/machinelearningbook/archivos/Social_Network_Ads3.csv')
+
+
+dataset = dataset[3:5]
+
+
+set.seed(123)
+split = sample.split( dataset$Purchased, SplitRatio = 0.8 )
+training_set = subset( dataset, split == TRUE  )
+testing_set = subset( dataset, split == FALSE  )
+
+
+
+training_set[,1:2] = scale( training_set[,1:2] )
+testing_set[,1:2] = scale( testing_set[,1:2] )
+
+
+
+classifier = svm(formula = Purchased ~ ., data = training_set, type="C-classification", kernel="radial")
+
+
+prob_pred = predict( classifier, newdata =  testing_set[,-3])
+
+
+
+cm = table(testing_set[, 3], y_pred)
+
+# 
+
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# 
+
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'Kernel SVM (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
