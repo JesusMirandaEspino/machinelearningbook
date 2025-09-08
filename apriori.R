847dcefaa9bdb46fe754b18b23348acf0be64fd2
GeraltJM <jesus.voluntadheredada.human@gmail.com>
2025-09-08 15:31:57 -0600

    a priori

---
+++ apriori.R
@@ -0,0 +1,4 @@
+
+library(arules)
+dataset = read.csv('C:/Users/jesus/IA/machinelearning/machinelearningbook/archivos/Market_Basket_Optimisation.csv', header = FALSE)
+#dataset = read.transactions('C:/Users/jesus/IA/machinelearning/machinelearningbook/archivos/Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
